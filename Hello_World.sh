bbdd9763cdec4b3a7072db05ee22a5919a219b24
CWalshTUD <c21391701@mytudublin.ie>
2023-09-29 13:11:53 +0100

    Hello World

---
+++ Hello_World.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+# A bash script to say Hello World for DevOps lab2
+# This script is just a sample script to demonstrate learning of git
+# Author: Christopher Walsh
+# Date: 29/9/23
+
+echo "Hello World"
